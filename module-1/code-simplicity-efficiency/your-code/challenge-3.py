b2c1230cc584fd427093b65667abb45f7e62629c
Silvia Martinez Valero <valeromsilvia@gmail.com>
2023-10-31 14:22:42 +0100

    challenge3-finished

--- module-1/code-simplicity-efficiency/your-code/challenge-3.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-3.py
@@ -1,30 +1,9 @@
-"""
-You are presented with an integer number larger than 5. Your goal is to identify the longest side
-possible in a right triangle whose sides are not longer than the number you are given.
-
-For example, if you are given the number 15, there are 3 possibilities to compose right triangles:
-
-1. [3, 4, 5]
-2. [6, 8, 10]
-3. [5, 12, 13]
-
-The following function shows one way to solve the problem but the code is not ideal or efficient.
-Refactor the code based on what you have learned about code simplicity and efficiency.
-"""
-
-def my_function(X):
-    solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
-                    solutions.append([x, y, z])
+def triangle(max_len):
+    solution = [[x,y,z] for z in range(3, max_len) for y in range(4, max_len) for x in range(5, max_len) if (x * x == y*y+z*z)]
     m = 0
-    for solution in solutions:
-        if m < max(solution):
-            m = max(solution)
+    for s in solution:
+        if m < max(s):
+            m = max(s)
     return m
-
-X = input("What is the maximal length of the triangle side? Enter a number: ")
-
-print("The longest side possible is " + str(my_function(int(X))))
+max_len = int(input("What is the maximal length of the triange side? "))
+print(f"The longest length possible for a side of this triange is {triangle(max_len)}")
