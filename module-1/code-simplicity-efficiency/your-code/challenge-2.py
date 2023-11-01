17378a22ad7d88b48bf3ce44e81cd0966973b769
Andrew Bavuels <andrew.bavuels@gmail.com>
2023-11-01 13:09:09 +0100

    Challenge 2 con caractéres adicionales (mayúsculas y símbolos)

--- module-1/code-simplicity-efficiency/your-code/challenge-2.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-2.py
@@ -7,32 +7,45 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+# Las librerias deben ejecutarse de primero, dentro de las buenas prácticas
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+import random
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+# Variables globales después de las librerías
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+def random_strings(pred_len = 12, mixed_characters=None):
+
+    lower_letters = ['a','b','c','d','e','f','g','h','i','j','k','l',
+                     'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    
+    numbers = ['0','1','2','3','4','5','6','7','8','9']
+
+    capital_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                       'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+
+    symbols = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', 
+                  '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"'] 
+                    
+
+    if mixed_characters is None:
+        mixed_characters = lower_letters + numbers + capital_letters + symbols
+
+    str_generation_counter = 0
+
+    string = ''
+
+    while str_generation_counter < pred_len:        
+        string += random.choice(mixed_characters)
+        str_generation_counter += 1
+    return string
+
+
+def string_groups(number_of_strings, min_length=8, max_length=12):
+    strings = [random_strings(random.randint(min_length, max_length)) for i in range(number_of_strings)]
+    return strings
+
+min_length = int(input('Enter minimum string length: '))
+max_length = int(input('Enter maximum string length: '))
+number_of_strings = int(input('How many random strings to generate? '))
+
+print(string_groups(number_of_strings, min_length, max_length))
