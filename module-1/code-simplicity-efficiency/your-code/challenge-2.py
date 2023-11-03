77164e9d3730c0cf2d8db8c4166bd5dd7cfd99c3
“Ana <teamurjc@gmail.com>
2023-11-03 19:09:33 +0100

    challenge 2 completed

--- module-1/code-simplicity-efficiency/your-code/challenge-2.py
+++ module-1/code-simplicity-efficiency/your-code/challenge-2.py
@@ -6,33 +6,27 @@ the strings being generated.
 The code is functional but has a lot of room for improvement. Use what you have learned
 about simple and efficient code, refactor the code.
 """
+import random as rd
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+class IncorrectLengthsError(Exception):
+    pass
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+def RandomStringGenerator(length=12, characters='abcdefghijklmnopqrstuvwxyz0123456789'):
+    return ''.join(rd.choice(characters) for _ in range(length))
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+def BatchStringGenerator(num_strings, min_length=8, max_length=12):
+    if min_length > max_length:
+        raise IncorrectLengthsError('Incorrect min and max string lengths. Try again.')
+    
+    return [RandomStringGenerator(rd.randint(min_length, max_length)) for _ in range(num_strings)]
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+if __name__ == '__main__':
+    a = int(input('Enter minimum string length: '))
+    b = int(input('Enter maximum string length: '))
+    n = int(input('How many random strings to generate? '))
+    
+    try:
+        generated_strings = BatchStringGenerator(n, a, b)
+        print(generated_strings)
+    except IncorrectLengthsError as e:
+        print(e)
