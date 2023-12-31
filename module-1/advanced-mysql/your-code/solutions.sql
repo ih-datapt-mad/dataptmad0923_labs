af244ce3eec3ef832849b8d9b1cf10df57d5352b
Silvia Martinez Valero <valeromsilvia@gmail.com>
2023-12-31 18:21:11 +0100

    errors fixed

--- module-1/advanced-mysql/your-code/solutions.sql
+++ module-1/advanced-mysql/your-code/solutions.sql
@@ -15,35 +15,15 @@ JOIN
 sales s ON t.title_id = s.title_id;
 
 -- Step 2
-SELECT 
-    ta.title_id AS TITLE_ID,
-    a.au_id AS AUTHOR_ID,
-    (SELECT SUM(t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100)
-     FROM sales s
-     WHERE s.title_id = ta.title_id
-     GROUP BY s.title_id, a.au_id) 
-     AS TOTAL_ROYALTIES
-FROM
-    titleauthor ta 
-JOIN 
-    authors a ON ta.au_id = a.au_id
-JOIN
-    titles t ON ta.title_id = t.title_id;
-
--- Step 3
 SELECT
-    a.au_id AS AUTHOR_ID,
-    SUM(total_royalty_advance) AS TOTAL_PROFITS
+    subquery.AUTHOR_ID AS AUTHOR_ID,
+    subquery.TITLE_ID AS TITLE_ID,
+    SUM(subquery.TOTAL_ROYALTIES) AS TOTAL_ROYALTIES
 FROM
-    authors a
-JOIN
-    titleauthor ta ON a.au_id = ta.au_id
-LEFT JOIN (
-    SELECT
-        ta.au_id,
-        ta.title_id,
-        (t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) +
-            (t.advance * ta.royaltyper / 100) AS total_royalty_advance
+    (SELECT
+        ta.au_id AS AUTHOR_ID,
+        ta.title_id AS TITLE_ID,
+        SUM(t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) AS TOTAL_ROYALTIES
     FROM
         titleauthor ta
     JOIN
@@ -51,10 +31,30 @@ LEFT JOIN (
     JOIN
         sales s ON t.title_id = s.title_id
     GROUP BY
-        ta.au_id, ta.title_id, total_royalty_advance
-) AS subquery ON a.au_id = subquery.au_id
+        ta.au_id, t.title_id) AS subquery 
 GROUP BY
-    a.au_id
+    subquery.AUTHOR_ID, subquery.TITLE_ID;
+
+-- Step 3
+SELECT
+    subquery.AUTHOR_ID AS AUTHOR_ID,
+    SUM(subquery.TOTAL_ROYALTIES) + SUM(TOTAL_ADVANCE) AS TOTAL_PROFITS
+FROM
+    (SELECT
+        ta.au_id AS AUTHOR_ID,
+        ta.title_id,
+        SUM(t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) AS TOTAL_ROYALTIES,
+		SUM(t.advance * ta.royaltyper / 100) AS TOTAL_ADVANCE
+    FROM
+        titleauthor ta
+    JOIN
+        titles t ON ta.title_id = t.title_id
+    JOIN
+        sales s ON t.title_id = s.title_id
+    GROUP BY
+        ta.au_id, t.title_id) AS subquery 
+GROUP BY
+    subquery.AUTHOR_ID
 ORDER BY
     TOTAL_PROFITS DESC
 LIMIT 3;
